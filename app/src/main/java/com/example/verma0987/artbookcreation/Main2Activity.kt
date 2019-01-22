05c2ce8164bb4dcbb211eb40bf51865aae73c4ff
rishabh verma <vermarishabh0987@gmail.com>
2019-01-22 20:08:39 +0530

    adding user permission for our images .. to let user add images from the phots/gallery (*NOTE = save fuctionality is not yet codded)

--- app/src/main/java/com/example/verma0987/artbookcreation/Main2Activity.kt
+++ app/src/main/java/com/example/verma0987/artbookcreation/Main2Activity.kt
@@ -1,6 +1,13 @@
 package com.example.verma0987.artbookcreation
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main2.*
+import java.util.jar.Manifest
 
 
 class Main2Activity : AppCompatActivity() {
@@ -8,6 +15,57 @@ class Main2Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+
+    fun select(view: View){
+        if(checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),2)
+        }else{
+            val intent =Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            startActivityForResult(intent,1)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+
+        if (requestCode == 2){
+            if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                val intent =Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent,1)
+            }
+        }
+
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null){
+            val image = data.data
+
+            try{
+                val selectedImage = MediaStore.Images.Media.getBitmap(this.contentResolver,image)
+                imageView.setImageBitmap(selectedImage)
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+
+
+
+
+        }
+
+
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+
+
+
+
+    fun save(view: View){
+
     }
 
 
