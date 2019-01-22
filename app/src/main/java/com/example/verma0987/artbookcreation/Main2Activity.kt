a1ccccd1926ce760a2ddabb27aaf41e0fe5a697a
rishabh verma <vermarishabh0987@gmail.com>
2019-01-22 19:12:07 +0530

    making menu adding inflator

---
+++ app/src/main/java/com/example/verma0987/artbookcreation/Main2Activity.kt
@@ -0,0 +1,17 @@
+package com.example.verma0987.artbookcreation
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+
+
+
+}
+
--- app/src/main/java/com/example/verma0987/artbookcreation/MainActivity.kt
+++ app/src/main/java/com/example/verma0987/artbookcreation/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.verma0987.artbookcreation
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +14,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val inflator : MenuInflater = menuInflater
+        inflator.inflate(R.menu.art,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        if(item?.itemId == R.id.addart){
+            val intent = Intent(applicationContext,Main2Activity::class.java)
+            startActivity(intent)
+
+        }
+
+
+        return super.onOptionsItemSelected(item)
     }
 }
